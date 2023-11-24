1d93503d8886d073f3fc88ddc6f94f209ae2d53e
ByeoungJoon Jeon <juni8453@naver.com>
2023-11-24 12:10:20 +0900

    project init

---
+++ src/main/java/com/emotionmusicnote/EmotionmusicnoteApplication.java
@@ -0,0 +1,13 @@
+package com.emotionmusicnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionmusicnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionmusicnoteApplication.class, args);
+	}
+
+}
