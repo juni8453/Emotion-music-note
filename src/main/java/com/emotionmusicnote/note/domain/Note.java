fcab24d0bcbb2391ef41954e20657d393dd027b9
ByeoungJoon Jeon <juni8453@naver.com>
2023-11-24 14:36:19 +0900

    :sparkles: Note Entity 생성
    - Note Entity 생성
    - id, emotion(그날의 감정), content(메모 내용) 속성

---
+++ src/main/java/com/emotionmusicnote/note/domain/Note.java
@@ -0,0 +1,33 @@
+package com.emotionmusicnote.note.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Note {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String emotion;
+
+  @Column(nullable = false)
+  private String content;
+
+  @Builder
+  public Note(String emotion, String content) {
+    this.emotion = emotion;
+    this.content = content;
+  }
+}
