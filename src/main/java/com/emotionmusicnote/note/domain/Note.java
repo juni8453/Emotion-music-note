fedf3e93a282d9ba4de8c4f2b0503cd5d3f3c29e
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-05 16:40:05 +0900

    :sparkles: Note, User 연관관계 추가
    - 여러 노트는 하나의 유저를 가지므로 ManyToOne 관계 추가
    - 연관관계 메서드 addUser() 추가

--- src/main/java/com/emotionmusicnote/note/domain/Note.java
+++ src/main/java/com/emotionmusicnote/note/domain/Note.java
@@ -1,11 +1,16 @@
 package com.emotionmusicnote.note.domain;
 
 import com.emotionmusicnote.common.BaseTime;
+import com.emotionmusicnote.user.domain.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,12 +28,21 @@ public class Note extends BaseTime {
   @Column(nullable = false)
   private String emotion;
 
+  @Lob
   @Column(nullable = false)
   private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
 
   @Builder
   public Note(String emotion, String content) {
     this.emotion = emotion;
     this.content = content;
+  }
+
+  public void addUser(User createUser) {
+    this.user = createUser;
   }
 }
