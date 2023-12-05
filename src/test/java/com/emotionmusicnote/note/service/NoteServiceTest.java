e3ded17e128458155b8fdb9c8608b785ba11a563
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-05 16:50:24 +0900

    :white_check_mark: NoteServiceTest 수정
    - 더미 데이터로 User 를 만들고, Session 에 저장해 테스트 전 로그인 상태를 만들어주도록 수정
    - session 데이터를 적용한 테스트 코드 수정

--- src/test/java/com/emotionmusicnote/note/service/NoteServiceTest.java
+++ src/test/java/com/emotionmusicnote/note/service/NoteServiceTest.java
@@ -5,11 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.emotionmusicnote.note.controller.request.NoteSaveRequest;
 import com.emotionmusicnote.note.controller.response.NoteSingleReadResponse;
 import com.emotionmusicnote.note.domain.NoteRepository;
+import com.emotionmusicnote.user.domain.User;
+import com.emotionmusicnote.user.domain.UserRepository;
+import com.emotionmusicnote.user.oauth.OAuthProvider;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 
 @SpringBootTest
 class NoteServiceTest {
@@ -20,6 +25,26 @@ class NoteServiceTest {
   @Autowired
   private NoteRepository noteRepository;
 
+  @Autowired
+  private UserRepository userRepository;
+
+  private final MockHttpSession session = new MockHttpSession();
+
+  @Order(value = 1)
+  @BeforeEach
+  void saveDummyData() {
+    User user = User.builder()
+        .nickname("dummy_nickname")
+        .providerId("dummy_providerId")
+        .profileImageUrl("dummy_profileImageUrl")
+        .oAuthProvider(OAuthProvider.KAKAO)
+        .build();
+
+    User saveUser = userRepository.save(user);
+    session.setAttribute("user", saveUser);
+  }
+
+  @Order(value = 2)
   @BeforeEach
   public void db_init() {
     noteRepository.deleteAll();
@@ -37,12 +62,13 @@ class NoteServiceTest {
         .build();
 
     // when
-    noteService.save(request);
+    noteService.save(request, session);
   }
 
   @Test
   void read() {
     // given
+    User loginUser = (User) session.getAttribute("user");
     String emotion = "슬픔";
     String content = "내용 테스트";
 
@@ -51,15 +77,16 @@ class NoteServiceTest {
         .content(content)
         .build();
 
-    Long saveNoteId = noteService.save(request);
+    Long saveNoteId = noteService.save(request, session);
 
     // when
-    NoteSingleReadResponse response = noteService.read(saveNoteId);
+    NoteSingleReadResponse response = noteService.read(saveNoteId, session);
 
     // then
     assertThat(response.getEmotion()).isEqualTo(emotion);
     assertThat(response.getContent()).isEqualTo(content);
     assertThat(response.getCreateAt()).isBefore(LocalDateTime.now());
     assertThat(response.getModifiedAt()).isBefore(LocalDateTime.now());
+    assertThat(response.getNoteWriterResponse().getNickname()).isEqualTo(loginUser.getNickname());
   }
 }
