167f0620035105ee3a57fe9b5bdfde6e81ff155b
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-12 15:35:13 +0900

    :recycle: @EnableJpaAuditing Main 에서 제거
    - Slice Test 시 JPA Auditing 를 찾지 못하는 예외가 발생하기 때문에 BeanConfig 클래스로 @EnableJpaAuditing 어노테이션 이동

--- src/main/java/com/emotionmusicnote/EmotionmusicnoteApplication.java
+++ src/main/java/com/emotionmusicnote/EmotionmusicnoteApplication.java
@@ -2,14 +2,12 @@ package com.emotionmusicnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class EmotionmusicnoteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmotionmusicnoteApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EmotionmusicnoteApplication.class, args);
+  }
 
 }
