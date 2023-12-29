4f3ff6c246bfa285c7bc7f76bce44a4d8ca4f387
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-29 17:48:04 +0900

    :sparkles: Front 기본적인 레이아웃 구성 및 Fontawesome 추가
    - 레이아웃 구성
    - App.vue
      - MainContainer.vue
        - header
        - main
          - sidebar
          - section (내부에 <vue-router> 사용)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container-fluid">
+    <MainContainer />
+  </div>
 </template>
 
 <script>
+import MainContainer from "@/components/MainContainer";
 export default {
   name: 'App',
   components: {
+    MainContainer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
+  height: 100%;
+  margin: 10px;
 }
 </style>
--- frontend/src/json/router.js
+++ frontend/src/json/router.js
@@ -1,6 +1,23 @@
 import {createRouter, createWebHistory} from "vue-router";
+import UserLogin from "@/components/UserLogin";
+import KakaoJoin from "@/components/KakaoJoin";
+import HomePage from "@/components/HomePage";
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/login',
+    component: UserLogin,
+  },
+  {
+    // kakao redirect uri (인가 코드 받는 즉시 컴포넌트 생성)
+    path: '/oauth/kakao',
+    component: KakaoJoin,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
