04486a48b5f9ca6300b9fefb7fc33e1fa7730f04
ByeoungJoon Jeon <juni8453@naver.com>
2024-01-11 16:55:21 +0900

    :sparkles: MethodArgumentTypeMissmatchException, NoteFoundNoteException 예외 핸들러 추가
    - 핸들러 추가로 해당 예외 발생 시 클라이언트로 전송할 응답 객체 커스텀하게 추가

--- backend/src/main/java/com/emotionmusicnote/common/exception/ExceptionController.java
+++ backend/src/main/java/com/emotionmusicnote/common/exception/ExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ExceptionController {
@@ -23,5 +24,23 @@ public class ExceptionController {
     }
 
     return response;
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ExceptionResponse typeMismatch() {
+    return ExceptionResponse.builder()
+        .code(HttpStatus.BAD_REQUEST.value())
+        .message("잘못된 요청입니다.")
+        .build();
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(NotFoundNoteException.class)
+  public ExceptionResponse notFoundNote(NotFoundNoteException exception) {
+    return ExceptionResponse.builder()
+        .code(exception.getStatusCode())
+        .message(exception.getMessage())
+        .build();
   }
 }
