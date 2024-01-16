e9a31f7de18dcbb6724da0e1eeddb1512d4d091d
ByeoungJoon Jeon <juni8453@naver.com>
2024-01-16 21:23:05 +0900

    :recycle: MainContainer.vue CSS Refactoring
    - MainContainer.vue CSS Refactoring

--- frontend/src/components/MainContainer.vue
+++ frontend/src/components/MainContainer.vue
@@ -1,51 +1,48 @@
 <template>
-  <header>
-    <div class="home-name">
-      <router-link to="/" class="header-router-link">
+    <!-- HEADER -->
+    <div class="header-body">
+      <router-link class="royalblue-button-router-link" to="/">
         <font-awesome-icon :icon="['fas', 'quote-left']" /> Emotion Note <font-awesome-icon :icon="['fas', 'quote-right']" />
       </router-link>
-    </div>
 
-    <div>
       <div v-if="$store.state.sessionId === null">
-        <button class="nav-button">
-          <router-link to="/login" class="header-router-link">
+        <button class="royalblue-button">
+          <router-link class="royalblue-button-router-link" to="/login">
             <font-awesome-icon :icon="['fas', 'user-tie']" /> Login
           </router-link>
         </button>
       </div>
 
       <div v-else-if="$store.state.sessionId !== null">
-        <button class="nav-button">
-          <img class="profile-img" :src="$store.state.profileImageUrl" alt="profile Url">
-          {{ $store.state.nickname }} 님 반갑습니다 !
+        <button class="royalblue-button">
+          <img class="header-login-image" :src="$store.state.profileImageUrl" alt="profile Url">{{ $store.state.nickname }} 님 반갑습니다 !
         </button>
-        <button class="nav-button" @click="logout">
-          로그아웃
-        </button>
+        <button class="royalblue-button" @click="logout">로그아웃</button>
       </div>
     </div>
-  </header>
 
-  <main>
-    <div class="sidebar">
-      <h4 class="sidebar-name">메뉴</h4>
-      <button class="sidebar-button">
-        <router-link to="/notes" class="sidebar-router-link">
-          <font-awesome-icon icon="clipboard" /> - 일기 목록
-        </router-link>
-      </button>
-      <button class="sidebar-button">
-        <router-link to="/note/new" class="sidebar-router-link">
-          <font-awesome-icon icon="pen-to-square" /> - 일기 작성
-        </router-link>
-      </button>
+    <div class="main-body">
+      <!-- SIDEBAR -->
+      <div class="sidebar-body">
+        <h4>메뉴</h4>
+        <button class="white-button">
+          <router-link class="white-button-router-link" to="/notes">
+            <font-awesome-icon icon="clipboard" /> - 일기 목록
+          </router-link>
+        </button>
+        <button class="white-button">
+          <router-link class="white-button-router-link" to="/note/new">
+            <font-awesome-icon icon="pen-to-square" /> - 일기 작성
+          </router-link>
+        </button>
+      </div>
+
+      <!-- MAIN MENU -->
+      <div class="component-body">
+        <router-view></router-view>
+      </div>
     </div>
 
-    <div class="section">
-      <router-view></router-view>
-    </div>
-  </main>
 </template>
 
 <script>
@@ -65,39 +62,19 @@ export default {
 </script>
 
 <style scoped>
-header {
-  background-color: royalblue;
-  padding: 10px;
+@import '../assets/CommonStyle.css';
+
+.header-body {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-radius: 8px;
-  margin-bottom: 10px;
-}
-
-.header-router-link {
-  text-decoration: none;
-  color: white;
-}
-
-.home-name {
-  color: white;
-}
-
-.nav-button {
-  color: white;
-  border-radius: 6px;
-  border: none;
   background-color: royalblue;
+  border-radius: 8px;
+  padding: 10px;
+  margin-bottom: 15px;
 }
 
-.nav-button:hover {
-  color: white;
-  border-radius: 6px;
-  background-color: #ef4a4a;
-}
-
-.profile-img {
+.header-login-image {
   width: 50px;
   height: 50px;
   border-radius: 60%;
@@ -106,53 +83,29 @@ header {
   margin-right: 7px;
 }
 
-main {
-  display: flex;
-}
-
-.sidebar {
-  background-color: #ffffff;
-  padding: 10px;
-  border-radius: 8px;
-  width: 170px;
+.sidebar-body {
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 170px;
+  height: 200px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  border-right: 3px solid royalblue;
+  padding: 10px;
 }
 
-.sidebar-name {
-  text-align: center;
-  margin-bottom: 20px;
-  font-weight: bolder;
+.main-body {
+  display: flex;
 }
 
-.sidebar-button {
-  background-color: white;
-  color: black;
-  margin-bottom: 10px;
-  border: none;
-  border-radius: 4px;
-  font-weight: bolder;
-  cursor: pointer;
-  height: 45px;
-  width: 100%;
-  text-align: center;
-}
-
-.sidebar-button:hover {
-  background-color: #898989;
-}
-
-.sidebar-router-link {
-  text-decoration: none;
-  color: black;
-}
-
-.section {
+.component-body {
+  flex: 1;
+  border-radius: 8px;
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
   padding: 20px;
   margin-left: 20px;
-  flex: 1;
-  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
-  border-radius: 8px;
 }
 
 </style>
