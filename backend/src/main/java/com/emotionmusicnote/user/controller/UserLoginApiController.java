c876849930fa8b23c332f699e0f0518060337ec2
ByeoungJoon Jeon <juni8453@naver.com>
2024-02-08 16:04:13 +0900

    :recycle: WebConfig 수정 및 Session 체크 End-Point 추가
    - /api/check-server-state 경로는 Interceptor 를 타지 않도록 수정
    - /api/check-session 경로를 Interceptor 를 타도록 해서 세션이 유효하지 않다면 SessionExpiredException 예외를 발생할 수 있도록 작성

--- backend/src/main/java/com/emotionmusicnote/user/controller/UserLoginApiController.java
+++ backend/src/main/java/com/emotionmusicnote/user/controller/UserLoginApiController.java
@@ -16,7 +16,7 @@ public class UserLoginApiController {
   private final UserLoginService userLoginService;
 
   // 프론트에서 보낸 인가 코드를 통해 카카오 서버에서 code 를 받아 로그인 처리
-  @GetMapping("api/login/{code}")
+  @GetMapping("/api/login/{code}")
   public ResponseEntity<SignInUserInfo> login(
       @PathVariable("code") String code, HttpSession session) {
     SignInUserInfo signInUserInfo = userLoginService.login(code, session);
@@ -24,8 +24,13 @@ public class UserLoginApiController {
     return ResponseEntity.ok(signInUserInfo);
   }
 
-  @GetMapping("api/check-server-state")
+  @GetMapping("/api/check-server-state")
   public String checkServerState() {
     return "Server is up and running";
+  }
+
+  @GetMapping("/api/check-session")
+  public String checkSession() {
+    return "Session OK";
   }
 }
