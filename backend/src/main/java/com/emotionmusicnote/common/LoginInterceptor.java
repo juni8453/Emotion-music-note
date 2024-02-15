50f670a30f891fe25d8e54bb3974b80d50aea79b
ByeoungJoon Jeon <juni8453@naver.com>
2024-02-15 17:19:19 +0900

    :sparkles: 로그 DB 관련 인터셉터 설정 추가
    - 모든 API 접근 이후 어떤 응답을 받았는지 로그 DB Table 에 저장하기 위해 afterCompletion() 오버라이딩해서 사용
    
    - LogHistoryRepository 를 의존 주입함에 따라 인터셉터 등록하는 객체의 매개변수로 LogHistoryRepository 객체 주입

--- backend/src/main/java/com/emotionmusicnote/common/LoginInterceptor.java
+++ backend/src/main/java/com/emotionmusicnote/common/LoginInterceptor.java
@@ -1,15 +1,24 @@
 package com.emotionmusicnote.common;
 
 import com.emotionmusicnote.common.exception.SessionExpiredException;
+import com.emotionmusicnote.history.domain.LogHistory;
+import com.emotionmusicnote.history.domain.LogHistoryRepository;
+import com.emotionmusicnote.user.domain.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@RequiredArgsConstructor
 public class LoginInterceptor implements HandlerInterceptor {
 
+  private final LogHistoryRepository logHistoryRepository;
+
   @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+      Object handler) {
 
     if (request.getMethod().equals("OPTIONS")) {
       System.out.println("Preflight Request");
@@ -23,5 +32,26 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     return true;
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) {
+
+    String httpMethod = request.getMethod();
+    String requestUri = request.getRequestURI();
+    int responseStatus = response.getStatus();
+    User user = (User) request.getSession().getAttribute("user");
+
+    LogHistory logHistory = LogHistory.builder()
+        .userId(user.getId())
+        .nickname(user.getNickname())
+        .httpMethod(httpMethod)
+        .requestUri(requestUri)
+        .responseStatus(responseStatus)
+        .callTime(LocalDateTime.now())
+        .build();
+
+    logHistoryRepository.save(logHistory);
   }
 }
